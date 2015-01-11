7ffafe757ed4a30489189de7f1f79c8fd8d41aca
Niels Dequeker <niels.dequeker@gmail.com>
2015-01-11 15:30:34 +0100

    Project setup

---
+++ gulpfile.js
@@ -0,0 +1,59 @@
+'use strict';
+
+var gulp    = require('gulp'),
+    traceur = require('gulp-traceur'),
+    connect = require('gulp-connect'),
+    open    = require('open');
+
+gulp.task('connect', function () {
+  var livereloadPort = 39729;
+
+  connect.server({
+    port: 8080,
+    livereload: {
+      port: livereloadPort
+    },
+    middleware: function (connect) {
+      function mountFolder(connect, dir) {
+        return connect.static(require('path').resolve(dir));
+      }
+
+      return [
+        require('connect-livereload')({ port: livereloadPort }),
+        mountFolder(connect, 'app')
+      ];
+    }
+  });
+});
+
+gulp.task('watch', function () {
+  gulp.watch([
+    'app/**/*.html',
+    'app/**/*.js',
+    '!app/compiled/**'
+  ], function (event) {
+    return gulp.src(event.path)
+      .pipe(connect.reload());
+  });
+
+  gulp.watch([
+    'app/**/*.js'
+  ], ['compile']);
+});
+
+gulp.task('compile', function () {
+  return gulp.src([
+      'app/**/*.js',
+      '!app/compiled/**'
+    ])
+    .pipe(traceur())
+    .pipe(gulp.dest('app/compiled'));
+});
+
+gulp.task('open', ['connect'], function () {
+  require('open')('http://localhost:9421');
+});
+
+gulp.task('serve', function () {
+  gulp.start('connect', 'watch', 'compile', 'open');
+});
